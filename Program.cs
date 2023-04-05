1778d1c7a78c65fcfac083b195463b9b9e7a3050
Анатолий <Chepanov199287@gmail.com>
2023-04-05 22:52:16 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillFactory_5_3_1
+{
+    class Program
+    {
+        static int ChangeAge(int age)
+        {
+            age = age + 1;           
+            return age;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите Ваш возраст");
+
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            int Age = ChangeAge(age);
+
+            Console.WriteLine(Age);
+
+            Console.ReadKey();
+        }
+    }
+}
